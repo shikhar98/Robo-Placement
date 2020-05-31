1fca2fc3a316b821b433d3ee13b7124b2709fe00
Shikhar Arora <shikhararora98@gmail.com>
2020-05-31 21:31:43 +0530

    hosting commit 3

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 var mongoose = require('mongoose');
 
-var options = {promiseLibrary: require('bluebird'), useNewUrlParser: true};
-require('dotenv').config()
+var options = { promiseLibrary: require('bluebird'), useNewUrlParser: true };
+require('dotenv').config();
 
 //mLab credentials
 // var mongodbUri = process.env.DATABASEURL;
 // console.log(process.env.DATABASEURL);
 
- var mongodbUri = 'mongodb://localhost:27017/robo';
+var mongodbUri = 'mongodb+srv://admin:admin@cluster0-t2n1v.mongodb.net/test?retryWrites=true&w=majority';
 
 mongoose.connect(mongodbUri, options);
 var conn = mongoose.connection;
